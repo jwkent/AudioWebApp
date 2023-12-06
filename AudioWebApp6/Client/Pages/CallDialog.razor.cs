ed9df4d8476cf8de81bac23240cb14547629b296
Benjamin Kent <benjaminfranklinkent@gmail.com>
2023-12-05 19:37:24 -0800

    Add Razor pages with navigation and dialog for call in feature.

---
+++ AudioWebApp6/Client/Pages/CallDialog.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AudioWebApp.Client.Pages
+{
+    public partial class CallDialog : ComponentBase
+    {
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+
+        void Cancel() => MudDialog.Cancel();
+        private string telephoneNumber = "tel:123-456-7890";
+    }
+}
---
+++ AudioWebApp6/Client/Pages/Radio.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+
+using static MudBlazor.CategoryTypes;
+
+namespace AudioWebApp.Client.Pages
+{
+    public partial class Radio : Microsoft.AspNetCore.Components.ComponentBase
+    {
+        bool _radioPlaying;
+        
+        private void PlayRadio()
+        {
+            _radioPlaying = !_radioPlaying;
+        }
+
+        DialogOptions disableBackdropClick = new DialogOptions()
+        { DisableBackdropClick = true };
+        private void OpenDialog()
+        {
+            if (_radioPlaying) { _radioPlaying =  false; };
+
+            DialogService.Show<CallDialog>();
+        }
+    }
+}
