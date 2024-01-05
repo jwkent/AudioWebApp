9703f3e62cf1c79a7f089c8f92c70e53489aa491
Benjamin Kent <benjaminfranklinkent@gmail.com>
2024-01-04 21:05:09 -0800

    Complete audio element

--- AudioWebApp6/Client/Services/SharedDataService.cs
+++ AudioWebApp6/Client/Services/SharedDataService.cs
@@ -4,5 +4,11 @@
     {
         public string? AudioLink { get; set; }
         public string? AudioTitle { get; set; }
+        public event Action OnPlayerToggle;
+
+        public void TogglePlayer()
+        {
+            OnPlayerToggle?.Invoke();
+        }
     }
 }
