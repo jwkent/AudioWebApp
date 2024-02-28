9c48b7ffe421ff80e36c7ea33dc93761c3777b57
benjkent <68797909+benjkent@users.noreply.github.com>
2024-02-28 15:01:49 -0800

    Add AnnouncementsFileWriter that writes scrapped Announcements to file.

---
+++ AudioWebApp6/Shared/Models/AnnouncementData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioWebApp.Shared.Models
+{
+    public class AnnouncementData
+    {
+        public DateTime DateStamp { get; set; }
+        public Announcement[] Announcements { get; set; }
+
+        public AnnouncementData( Announcement[] announcements)
+        {
+            DateStamp = DateTime.Now;
+            Announcements = announcements;
+        }
+    }
+}
