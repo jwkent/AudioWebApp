bd485ee6cdc8fddcc74c66e915ce996eda168b9e
benjkent <68797909+benjkent@users.noreply.github.com>
2024-03-05 09:01:35 -0800

    Add Announcement.razor page

--- AudioWebApp6/Client/Pages/Index.razor.cs
+++ AudioWebApp6/Client/Pages/Index.razor.cs
@@ -5,12 +5,6 @@ namespace AudioWebApp.Client.Pages
 {
     public partial class Index: ComponentBase
     {
-        string? dataReady;
-        
-        protected override async Task OnInitializedAsync()
-        {
-            await announcementService.LoadAnnouncements();
-            dataReady = "dataLoaded";
-        }
+       
     }
 }
