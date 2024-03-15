e19dbb971625d8a73f1d907fcf87816f235f0b1c
benjkent <68797909+benjkent@users.noreply.github.com>
2024-03-15 14:14:45 -0700

    add some protection to DebateGroupUtility.

--- AudioWebApp6/Client/Utilities/DebateGrouperUtility.cs
+++ AudioWebApp6/Client/Utilities/DebateGrouperUtility.cs
@@ -10,36 +10,55 @@ namespace AudioWebApp.Client.Utilities
         private Dictionary<string, List<Message>> _DebateGroups = new Dictionary<string, List<Message>>();
         public ObservableCollection<DebateGroup> DebateCollection { get; set; } 
         public ObservableCollection<Series> OriginalSeriesCollection { get; set; }
+        private bool IsValidCollection = false;
+        private string collectionServer;
+        private string collectionAbbr;
         public void CreateCategories(ObservableCollection<Series> collection)
         {
-            DebateCollection = new ObservableCollection<DebateGroup>();
-            string collecionServer = collection[0].Server;
-            string collectionAbbr = collection[0].Abbr;
 
-            
-            // to get the list of messages ie debates in the collection param
-            List<Message> messages = new List<Message>();
-            foreach (Series series in collection)
+            if(collection.Count == 0)
             {
-                foreach(var message in series.Messages)
-                {
-                    Message alteredMessage = RemoveNumerical(message); // remove the numbers from the front of the name
-                    messages.Add(alteredMessage);
-                }  
+                IsValidCollection = false;             
             }
-            
-            // Now I have a list of debates as messages  <name and link>
-            foreach (var str in messages)
+            else
             {
-                string category = CategorizeString(str);  //put a message name in -> get back a category
-                if(!_DebateGroups.ContainsKey(category))
+                IsValidCollection = true;
+                DebateCollection = new ObservableCollection<DebateGroup>();
+                collectionServer = collection[0].Server;
+                collectionAbbr = collection[0].Abbr;
+            }
+
+            if (!IsValidCollection)
+            {
+                Console.WriteLine($"Sorry the collection of Debates is empty.");
+            }
+            else
+            {
+                // Create a list of the collections messages
+                List<Message> messages = new List<Message>();
+                foreach (Series series in collection)
                 {
-                    _DebateGroups[category] = new List<Message>();
+                    foreach (var message in series.Messages)
+                    {
+                        Message alteredMessage = RemoveNumerical(message); // remove the numbers from the front of the name
+                        messages.Add(alteredMessage);
+                    }
                 }
-                _DebateGroups[category].Add(str);
-            }
 
-            CreateDebateGroup(_DebateGroups, collecionServer, collectionAbbr);
+                //Use the Message Name property to sort into groups.
+                foreach (var str in messages)
+                {
+                    string category = CategorizeString(str);  //put a message name in -> get back a category
+                    if (!_DebateGroups.ContainsKey(category))
+                    {
+                        _DebateGroups[category] = new List<Message>();
+                    }
+                    _DebateGroups[category].Add(str);
+                }
+
+                CreateDebateGroup(_DebateGroups, collectionServer, collectionAbbr);
+            }
+           
         }
         private void CreateDebateGroup(Dictionary<string, List<Message>> categories, string server, string abbr )
         {
