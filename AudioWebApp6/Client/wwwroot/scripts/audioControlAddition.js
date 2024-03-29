53d5f95df575d343bcba1c480c0f3aa9eb354237
benjkent <68797909+benjkent@users.noreply.github.com>
2024-03-28 18:49:09 -0700

    write javascript

--- AudioWebApp6/Client/wwwroot/scripts/audioControlAddition.js
+++ AudioWebApp6/Client/wwwroot/scripts/audioControlAddition.js
@@ -6,3 +6,11 @@ function audioReverse() {
     const audioPlayer = document.getElementById("audioPlayer");
     audioPlayer.currentTime -= 10;
 }
+function audioRate() {
+    const audioPlayer = document.getElementById("audioPlayer");
+    console.log("The AUDIOspeed is " + audioPlayer.playbackRate);
+    //audioPlayer.playbackRate = speed;
+    //console.log("event fired");
+    //console.log("The speed is " + audioPlayer.playbackRate);
+
+}
