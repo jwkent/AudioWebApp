63ec2cf5d86626623f11188d81e03befa63adb68
benjkent <68797909+benjkent@users.noreply.github.com>
2024-03-28 19:43:44 -0700

    Update playbackspeed menu placement

--- AudioWebApp6/Client/wwwroot/scripts/audioControlAddition.js
+++ AudioWebApp6/Client/wwwroot/scripts/audioControlAddition.js
@@ -6,11 +6,8 @@ function audioReverse() {
     const audioPlayer = document.getElementById("audioPlayer");
     audioPlayer.currentTime -= 10;
 }
-function audioRate() {
+function audioPlaybackRate(speed) {
     const audioPlayer = document.getElementById("audioPlayer");
-    console.log("The AUDIOspeed is " + audioPlayer.playbackRate);
-    //audioPlayer.playbackRate = speed;
-    //console.log("event fired");
-    //console.log("The speed is " + audioPlayer.playbackRate);
+    audioPlayer.playbackRate = speed;
 
 }
