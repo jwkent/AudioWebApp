3eee014df3fa492db4ac006def3492efe6c6e681
benjkent <68797909+benjkent@users.noreply.github.com>
2024-05-20 23:29:10 -0700

    Add ShareLink page and share audio button

---
+++ AudioWebApp6/Client/Services/LinkShareService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+using System.Reflection;
+
+using System;
+using System.Web;
+
+namespace AudioWebApp.Client.Services
+{
+    public class LinkShareService
+    {
+        private readonly IJSRuntime _jsruntime;
+
+        public LinkShareService(IJSRuntime jSRuntime)
+        {
+            _jsruntime = jSRuntime;
+        }
+        public async Task CopyAudioLinkToClipboard(string title, string source)
+        {
+            string unicodeSource = HttpUtility.UrlEncode(source);
+            Console.WriteLine("encoded: " + unicodeSource);
+            string linkParams = $"{title}/{unicodeSource}";
+            string link = "https://localhost:8001/sharedplayer/" + linkParams;
+            
+            await _jsruntime.InvokeVoidAsync("shareAudioLink", link); 
+            Console.WriteLine(link);
+        }
+    }
+}
---
+++ AudioWebApp6/Client/wwwroot/scripts/shareLink.js
@@ -0,0 +1,8 @@
+﻿function shareAudioLink(link) {
+    navigator.clipboard.writeText(link).then(function () {
+        alert("Copied to clipboard");
+    })
+        .catch(function (error) {
+            alert(error);
+        });
+}
