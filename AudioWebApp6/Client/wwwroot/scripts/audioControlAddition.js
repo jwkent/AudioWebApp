1ac607685a71fc5ef51802d85ef5fd42e6896ed6
benjkent <68797909+benjkent@users.noreply.github.com>
2024-06-02 15:15:26 -0700

    Update playback speed to be stored between player sessions using localforage instance.

--- AudioWebApp6/Client/wwwroot/scripts/audioControlAddition.js
+++ AudioWebApp6/Client/wwwroot/scripts/audioControlAddition.js
@@ -9,7 +9,7 @@ function audioReverse() {
 function audioPlaybackRate(speed) {
     const audioPlayer = document.getElementById("audioPlayer");
     audioPlayer.playbackRate = speed;
-
+    storePlaybackSpeed(speed);
 }
 function shareAudioLink(link) {
     navigator.clipboard.writeText(link).then(function () {
@@ -24,4 +24,21 @@ function sendSMS(source, title) {
     var phoneNumber = "";
     //console.log("in js sendSMS");
     window.open(`sms:${phoneNumber}?body=${encodeURIComponent(source)}`);
+}
+function getPlaybackSpeed() {
+    var speedRate = localforage.createInstance({
+        name: "PlaybackSpeed"
+    });
+    speedRate.getItem("speedRate").then((value) => {
+        let storedSpeed = value !== null ? value : 1.0;
+        audioPlaybackRate(storedSpeed);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+function storePlaybackSpeed(userPreferredPlaybackSpeed) {
+    var playbackSpeedStore = localforage.createInstance({
+        name: "PlaybackSpeed"
+    });
+    playbackSpeedStore.setItem("speedRate", userPreferredPlaybackSpeed);
 }
