27a90c5fc4411cb046968887655151db20005819
benjkent <68797909+benjkent@users.noreply.github.com>
2024-06-13 10:16:11 -0700

    Update FavoriteQueue serverPath to empty string value. Disable Queue for audio books.

--- AudioWebApp6/Client/Services/SharedDataService.cs
+++ AudioWebApp6/Client/Services/SharedDataService.cs
@@ -11,7 +11,7 @@ namespace AudioWebApp.Client.Services
         public string? AudioTitle { get; set; }
         public event Action OnPlayerToggle;
         public event Action OnShowContent;
- 
+        public string? QueuedMessagesServerPath { get; set; }
         public void TogglePlayer()
         {
             OnPlayerToggle?.Invoke();
@@ -47,22 +47,14 @@ namespace AudioWebApp.Client.Services
         public List<Message>? QueuedMessages { get; set; }
         public void GenerateQueue(List<Message> queue, string message, string serverPath)
         {
-            
+            QueuedMessagesServerPath = serverPath;
             QueuedMessages = queue.SkipWhile(item => item.Name != message).Skip(1).ToList();
-            foreach(var q in queue)
-            {
-                q.Link = Path.Combine(serverPath, q.Link);
-            }
-
-            foreach(var i in QueuedMessages)
-            {
-                Console.WriteLine($"THis is the link: {i.Link}");
-            }
+            
         }
 
         public void GenerateFavoriteQueue(FavoritesService fs,string audioName)
         {
-
+            QueuedMessagesServerPath = string.Empty;
             IEnumerable<Favorite>? queue = fs.Favorites;
             
             List<Message> favoritesAsMessages = new List<Message>();
