2b90af16fe7978603210361007cb2eb1dfab3dcb
benjkent <68797909+benjkent@users.noreply.github.com>
2024-06-18 14:33:42 -0700

    Begin event listeners js and storage in local forage for end of message event.

---
+++ AudioWebApp6/Client/wwwroot/scripts/audioEventListener.js
@@ -0,0 +1,45 @@
+﻿function listenForEndOfMessage() {
+    const audioElement = document.getElementById("audioPlayer");
+
+    if (audioElement) {
+        setListener(audioElement);
+    } else {
+        setTimeout(() => {
+            console.log("Delayed exe after 1 second");
+            listenForEndOfMessage();
+        }, 1000);
+    }
+}
+function setListener(element) {
+    console.log("setting listener for " + element.getAttribute("title")); 
+    element.addEventListener('ended', () => {
+        console.log("The message has ended...");
+        //var completedMessageStore = localforage.createInstance({
+        //    name: "Completed"
+        //});
+        
+        //completedMessageStore.setItem(element.getAttribute("category"), element.getAttribute("title"));
+        storeCompletedMessage(element);
+        
+    })
+}
+function storeCompletedMessage(audioElement) {
+    var completedStore = localforage.createInstance({
+        name: "Complete"
+    });
+    try {
+        const existingValues = await completedStore.getItem(audioElement.getAttribute("category"));
+        if (existingValues) {
+            existingValues.push(audioElement.getAttribute("title"));
+            await completedStore.setItem(audioElement.getAttribute("category"), audioElement.getAttribute("title"));
+
+        } else {
+            await localforage.setItem(audioElement.getAttribute("category"), [audioElement.getAttribute("title")]);
+        }
+
+        console.log(`Value '${value}' updated or stored successfully for key '${key}'.`);
+    } catch (error) {
+        console.error(`Error updating or storing value for key '${key}': ${error.message}`);
+    }
+}
+
