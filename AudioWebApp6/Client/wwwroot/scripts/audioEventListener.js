35f4614da983122ea6362c40dfc47cf6052562e1
benjkent <68797909+benjkent@users.noreply.github.com>
2024-06-20 21:10:28 -0700

    Add category to series if it does not already exist.

--- AudioWebApp6/Client/wwwroot/scripts/audioEventListener.js
+++ AudioWebApp6/Client/wwwroot/scripts/audioEventListener.js
@@ -41,28 +41,57 @@ function storeCompletedMessage(audioElement) {
     var category = audioElement.getAttribute("category");
     var title = audioElement.getAttribute("title");
     var series = audioElement.getAttribute("series"); // The key used to store the data
-    var completedMessages = [];
-    var completedSeries = { category, completedMessages };
+
+    var seriesCollection = [];  // holds category objects [ (category)[messageTitles] ]
+
+    var categoryTitle;      // The category title of the messages
+
+    var completedMessages = []; //  messages
+
     var completedStore = localforage.createInstance({
         name: "CompletedStore"
     });
     completedStore.getItem(series).then((value) => {
         if (value == null) {
-            createNew(series, category, title, completedStore);
+            createNew(series, category, title, completedStore); // if there is no series. Topical, versebyverse, etc make one. Key...
         } else {
-            completedSeries = { category: value.category, completedMessages: value.completedMessages }
-            checkForCategory(series, category, title, completedSeries, completedStore)
+            // The series exists ( Topical, Verse by Verse, Books, Teachings. etc)
+            // Check if the CATEGORY is stored.
+            checkSeriesForCategoryTitle(series, category, title, value, completedStore)
         }
     }).catch((err) => { console.log(err); })
 }
-function createNew(key, category, title,instance) {
-    var completedMessages = [];
-    var completedSeries = { category, completedMessages }
-    completedMessages.push(title);
+function createNew(key, category, title, instance) {
     
-    instance.setItem(key, completedSeries);
+    var seriesCollection = [];
+    var categoryTitle;
+    var completedMessages = [];
+
+    completedMessages.push(title);
+    categoryTitle = category;
+    var object = { categoryTitle, completedMessages }
+    seriesCollection.push(object);
+    instance.setItem(key, seriesCollection);
 }
-function checkForCategory(key, category, title, completedSeries, instance) {
-    console.log(completedSeries);
-    // check if the category is in the completedSeries
+function checkSeriesForCategoryTitle(key, category, title, value, instance) {
+    console.log(value);
+    var categoryTitles = [];    //All the categories Titles (stored in db) for that series
+    var newSeriesCollection = [];
+    var storedCollection = []
+    for (var i = 0; i < value.length; i++) {
+        console.log(value[i].categoryTitle);
+        var cat = { categoryTitle: value[i].categoryTitle, completedMessages: value[i].completedMessages } // store the value 
+        storedCollection.push(cat);
+        categoryTitles.push(value[i].categoryTitle); //Use this to see if Category is already in db 
+    }
+    if (!categoryTitles.includes(category)) {
+        console.log("nope: " + category);
+
+        var object = { categoryTitle: category, completedMessages: title };
+        storedCollection.push(object);
+        
+        instance.setItem(key, storedCollection);
+    } else {
+        console.log("already got this category lets check if the title is there " + title); 
+    }
 }
